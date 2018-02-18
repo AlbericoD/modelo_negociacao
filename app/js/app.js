6df455b5333daf688295c3b236ec885add4b7603
AlbericoD <a.diasbarreto@icloud.com>
2018-02-18 12:04:54 -0300

    configuração inicial do typescript

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
-const n1 = new Negociacao(new Date(),2,100);
-const n2 = new Negociacao(new Date(),2,100);
-const n3 = new Negociacao(new Date(),3,100);
-const n4 = new Negociacao(new Date(),22,100);
-console.log(n1,n2,n3,n4);
+const n1 = new Negociacao(new Date(), 2, 100);
+const n2 = new Negociacao(new Date(), 2, 100);
+const n3 = new Negociacao(new Date(), 3, 100);
+const n4 = new Negociacao(new Date(), 22, 100);
+console.log(n1, n2, n3, n4);
