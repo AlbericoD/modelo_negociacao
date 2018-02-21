8a4d2f21e14dbd54cd4f62974c31f28e0c2e2e9c
AlbericoD <a.diasbarreto@icloud.com>
2018-02-20 23:40:33 -0300

    Alterado tipo generico e herança das classes View

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,6 +1,13 @@
-class View {
+class View<T> {
     protected _elemento: Element;
     constructor(seletor: string) {
         this._elemento = document.querySelector(seletor);
+    }
+    update(model:T){
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    template(model: T):string{
+        throw new Error('Você deve implemente o método template');
     }
 }
