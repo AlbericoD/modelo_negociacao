7101d075fcf69349d58c68ce1c7543d504d8a10d
AlbericoD <a.diasbarreto@icloud.com>
2018-02-24 11:17:58 -0300

    Alterado Generics da classe view

--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,6 +1,4 @@
 class MensagemView extends View<string> {
-
-    
      template(model: string):string{
          return `
             <p class="alert alert-info">${model}</p>
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,13 +1,10 @@
-class View<T> {
-    protected _elemento: Element;
+abstract class View<T> {
+    private _elemento: Element;
     constructor(seletor: string) {
         this._elemento = document.querySelector(seletor);
     }
     update(model:T){
         this._elemento.innerHTML = this.template(model);
     }
-
-    template(model: T):string{
-        throw new Error('Você deve implemente o método template');
-    }
+    abstract  template(model: T):string;
 }
